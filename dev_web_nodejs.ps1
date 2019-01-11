4a1d77912c1c88d743475ac449dd1d8a2fcf741f
Geoff Weinhold <geoff.weinhold@pacificlife.com>
2019-01-10 16:35:50 -0800

    commented out scripts

--- dev_web_nodejs.ps1
+++ dev_web_nodejs.ps1
@@ -2,7 +2,7 @@
 # Author: Microsoft
 # Common settings for web development with NodeJS
 
-Disable-UAC
+#Disable-UAC
 
 # Get the base URI path from the ScriptToCall value
 $bstrappackage = "-bootstrapPackage"
@@ -26,20 +26,20 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
-executeScript "WSL.ps1";
-executeScript "Browsers.ps1";
+#executeScript "WSL.ps1";
+#executeScript "Browsers.ps1";
 
 #--- Tools ---
 code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension msjsdiag.debugger-for-edge
+#code --install-extension msjsdiag.debugger-for-edge
 
 #--- Tools ---
 choco install -y nodejs-lts # Node.js LTS, Recommended for most users
-# choco install -y nodejs # Node.js Current, Latest features
-choco install -y visualstudio2017buildtools
-choco install -y visualstudio2017-workload-vctools
+#choco install -y nodejs # Node.js Current, Latest features
+#choco install -y visualstudio2017buildtools
+#choco install -y visualstudio2017-workload-vctools
 choco install -y python2 # Node.js requires Python 2 to build native modules
 
-Enable-UAC
-Enable-MicrosoftUpdate
-Install-WindowsUpdate -acceptEula
+#Enable-UAC
+#Enable-MicrosoftUpdate
+#Install-WindowsUpdate -acceptEula
